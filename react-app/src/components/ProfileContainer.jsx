3e694a6f6cf4102ab7feaab16feb46b7e45b6f34
Corbin-Petersen <corbincpetersen@gmail.com>
2025-05-13 17:20:11 -0500

    added user profiles header to profile container

--- react-app/src/components/ProfileContainer.jsx
+++ react-app/src/components/ProfileContainer.jsx
@@ -3,6 +3,7 @@ import ProfileCard from "./ProfileCard"
 export default function ProfileContainer() {
     return (
         <div className="profile-container">
+            <h2>USER PROFILES</h2>
             <ProfileCard name = "Steve" age = "42" bio = "Builds things from mined dirt and rock." />
             <ProfileCard name = "Mr. Bean" age = "60" bio = "Is a happy guy, lives alone, causes chaos wherever he goes." />
             <ProfileCard name = "Bubbles" age = "487" bio = "Son of Cthulu" />
